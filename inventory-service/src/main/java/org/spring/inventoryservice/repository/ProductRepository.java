0d2ad9e2a6360a6a11e461b98eb345617e4ffab5
Hamza-Azdoud <h.azdoud@etu.enset-media.ac.ma>
2025-02-26 00:16:31 +0000

    first commit

--- inventory-service/src/main/java/org/spring/inventoryservice/repository/ProductRepository.java
+++ inventory-service/src/main/java/org/spring/inventoryservice/repository/ProductRepository.java
@@ -3,5 +3,5 @@ package org.spring.inventoryservice.repository;
 import org.spring.inventoryservice.entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProductRepository extends JpaRepository<Product, Integer> {
+public interface ProductRepository extends JpaRepository<Product, String> {
 }
